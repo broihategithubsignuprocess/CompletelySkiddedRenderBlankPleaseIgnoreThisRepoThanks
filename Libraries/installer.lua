2ea9f0957a719a441ddc51eba5f858d1e92ac982
Sail100 <85443860+Sail100@users.noreply.github.com>
2024-03-03 19:30:03 -0500

    rexi is testing

--- Libraries/installer.lua
+++ Libraries/installer.lua
@@ -210,11 +210,12 @@ return (function(ria)
 		mainframe.Visible = true 
 		guiframe.Visible = false 
 		activated = true
+		--[[		
 		if httprequest == nil or base64_decode == nil or writefile == nil then 
 			progresstext.TextColor3 = Color3.fromRGB(255, 0, 0)
 			progresstext.Text = ('Render isn\'t supported for "'..executor..'".') 
 			return
-		end
+		end--]]
 		installing = tick()
 		for step, func in next, steps do 
 			progresstext.Text = titles[step]
